dcda01ade9bb639e9ddf9a6e77d6629ea5b6ca28
Simion <simion.okola@student.moringaschool.com>
2022-08-16 03:04:51 +0300

    Completd assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let multiply;
+let num1 = 31;
+let num2 = 2
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10);
+
+
+let mod;
+let num3 = 32;
+let num4 = 7;
+mod = num3 % num4;
+
+let max = Math.max(1, 4, 8, 9, 13, 17, 20 );
